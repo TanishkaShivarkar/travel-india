b9afa64e22d17592b22bf98f715b654deaa10291
Tanishka Shivarkar <tanishkas3112@gmail.com>
2025-11-10 19:23:20 +0530

    index.js page added

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import destinationRoutes from "./routes/destinationRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Travel India API is running ✅");
+});
+
+app.use("/api/destinations", destinationRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`🚀 Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log("❌ Mongo Error:", err));
