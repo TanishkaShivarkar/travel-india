9481f038362e5ad6b17134fb25278bf88feb309e
Tanishka Shivarkar <tanishkas3112@gmail.com>
2025-11-14 23:20:44 +0530

    About js page created

---
+++ client/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>About Us</h1>
+      <p>This project helps you explore beautiful destinations across India.</p>
+    </div>
+  );
+}
